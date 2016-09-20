dad221c6469cc065a9cd26caf075dad996c6d37a
Ranjeet Singh <ranjeetbhu88@gmail.com>
2016-09-20 23:36:01 +0530

    server.js eddited

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//http://localhost:8080/article-one
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
@@ -8,6 +9,22 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+app.get('/article-one', function (req, res) {
+	//res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+	res.send('Article one requested and will be served here');
+});
+
+
+app.get('/article-two', function (req, res) {
+         //res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+        res.send('Article two requested and will be served here');
+});
+
+ app.get('/article-three', function (req, res) {
+          //res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+         res.send('Article three requested and will be served here');
+ });
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
