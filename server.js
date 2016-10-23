ab819bf730c9c52f4ee265e7a0d5158585631b7e
rksin8 <ranjeetbhu88@gmail.com>
2016-10-23 22:46:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -204,10 +204,11 @@ app.get('/', function (req, res) {
 
 var pool = new Pool(config);
 
+/*
 app.get('/test-db', function (req, res) {
     pool.query("SELECT * from test", function(err,resulr){
         if(err){
-            res.status(500).send(err.toSting());
+            res.status(500).send(err.toString());
         }else{
             res.send(JSON.String(fy(results)));
         }
@@ -216,13 +217,27 @@ app.get('/test-db', function (req, res) {
     });
  
 });
+*/
 
 
 
-/*
-app.get('/:articleName', function (req, res) {
+app.get('/articles:articleName', function (req, res) {
     //articleName==article-one
   var articleName=req.params.articleName;
+  pool.query("SELECT * FROM article WHERE title= " +req.params.articleName, function(err,result){
+      if(err){
+         res.status(500).send(err.toString()); 
+      }else{
+          if(result.rows.length===0){
+            res.status(404).send("Article not found");   
+          }else{
+              var articleData=result.rows[0];
+              res.send(createTemplate(articleData));
+          }
+      }
+      
+      
+  });
   res.send(createTemplate(articles[articleName]));
 });
 
